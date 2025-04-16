bae730a2c45360d58eff04df4c21482cb887ae90
Natron16 <nl1101043@student.francistuttle.edu>
2025-04-16 10:52:49 -0500

    Made a new page

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a171b19a7c6f63e19dde2eabda0ed7de57b6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5332e02462f031026d1f76083d1d48d44c5f6f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
